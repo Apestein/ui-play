87a4a1da994dc98a5d0bd9ac9dd2a9898952f3c5
Apestein <ltn2057@protonmail.com>
2023-05-07 23:48:06 -0400

    save point

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { ExamplesNav } from "@/components/examples-nav"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="container">test</div>
+      <div className="container">
+        <ExamplesNav />
+      </div>
     </main>
   )
 }
