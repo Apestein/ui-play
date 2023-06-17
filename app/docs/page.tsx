6be8009958e0757b906d83efd2fb13bfc0d8f8b6
Apestein <ltn2057@protonmail.com>
2023-06-17 08:19:01 -0400

    save point

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,8 +1,13 @@
 import IndexDoc from "@/docs/index.mdx"
+import { DocsPageHeader } from "@/components/page-header"
 
 export default function DocsPage() {
   return (
-    <div className="flex-col rounded-lg border shadow-xl md:flex">
+    <div className="flex-col rounded-lg border p-10 shadow-xl md:flex">
+      <DocsPageHeader
+        heading="Docs Demo"
+        text="This is an example of a doc site built with MDX."
+      />
       <IndexDoc />
     </div>
   )
